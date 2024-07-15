3f9f1856c6c4177486fc353063c1c3c0e3bd70ee
Giulio Garbi <giulio.garbi@gmail.com>
2024-07-15 13:06:41 +0200

    Fix to lock for PTHREAD_MUTEX_ERRORCHECK

--- modules/lazyseqBnewschedule.c
+++ modules/lazyseqBnewschedule.c
@@ -113,12 +113,13 @@ int __cs_mutex_lock(__cs_mutex_t *__cs_mutex_to_lock, unsigned int __cs_thread_i
 	        *__cs_mutex_to_lock = ((__cs_thread_index+1)<<1)|1;
 	        __CSEQ_message("lock acquired");
 	    } else {
-	        if(*__cs_mutex_to_lock==-2){
-	            *__cs_mutex_to_lock = (__cs_thread_index+1)<<2;
-	            __CSEQ_message("lock acquired");
-	        } else {
+	        if(*__cs_mutex_to_lock == (__cs_thread_index+1)<<1) {
 	            return 16;
-	        }
+            } else {
+                __CSEQ_assume(*__cs_mutex_to_lock==-2);
+	            __CSEQ_message("lock acquired");
+                *__cs_mutex_to_lock = (__cs_thread_index+1)<<1;
+            }
 	    }
 	}
 	return 0;
